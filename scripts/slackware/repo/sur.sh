b387da0bd76d095098367fb52cf5b450a3f92902
simargl <dannasjedan@gmail.com>
2024-05-18 18:34:16 +0000

    Update sur.sh

--- scripts/slackware/repo/sur.sh
+++ scripts/slackware/repo/sur.sh
@@ -9,18 +9,17 @@ fn_create_sbo_packages() {
         if [ ! -f "$(basename $SLACKBUILD)" ]; then
             wget $SLACKBUILD
             tar -xf "$(basename $SLACKBUILD)"
-            i="$(basename $SLACKBUILD | sed s'/.tar.gz//'g)"
-            cd $i; source $PWD/$i.info
-            if [ ! -f "$(basename $DOWNLOAD)" ]; then
-                wget $DOWNLOAD
-            fi
-            sh $i.SlackBuild
-            cd -
-            fetch it $PRGNAM-$VERSION-*
         fi
+        i="$(basename $SLACKBUILD | sed s'/.tar.gz//'g)"
+        cd $i; source $PWD/$i.info
+        if [ ! -f "$(basename $DOWNLOAD)" ]; then
+            wget $DOWNLOAD
+        fi
+        sh $i.SlackBuild
+        cd -
+        fetch it $PRGNAM-$VERSION-*
     done
 }
 
 fetch it $(fetch sr /d/|grep -v Installed|cut -d '(' -f1)
 fn_create_sbo_packages "$@"
-
